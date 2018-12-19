f36609028d1a422360687f4b1cdf4b0584bcbe0c
iRadEntertainment <devitadario@gmail.com>
2018-12-19 17:29:48 +0100

    default_env.tres deleted

--- main.gd
+++ main.gd
@@ -15,5 +15,5 @@ func swipe_tab(amt): #transmit swipe to tabs
 	var tab_next = glb.current_tab + 1
 	var tab_prev = glb.current_tab - 1
 	if   amt < 0 and tab_prev < 0: return
-	elif amt > 0 and tab_prev > n_tabs.size()-1: return
+	elif amt > 0 and tab_next > n_tabs.size()-1: return
 	n_tabs[glb.current_tab].swipe(amt)
